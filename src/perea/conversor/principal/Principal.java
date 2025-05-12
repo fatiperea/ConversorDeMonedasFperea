f3bc148157f78345abae1d4c19a9c7e7a9b2956e
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-11 23:30:44 -0300

    agrego apikey, scaanner y mensajes

---
+++ src/perea/conversor/principal/Principal.java
@@ -0,0 +1,16 @@
+package perea.conversor.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        String apiKey="98940a4ca8e53fb94f327ebb";
+
+        Scanner lectura= new Scanner(System.in);
+
+        System.out.println("Soy la API Conversor de monedas y te doy la bienvenida");
+
+
+    }
+}
