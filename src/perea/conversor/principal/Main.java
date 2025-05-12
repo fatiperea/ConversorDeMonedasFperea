749f872ce16ac58b186323115e3e88e6419ee72d
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-12 12:46:57 -0300

    se logra invertir la conversion

--- src/perea/conversor/principal/Main.java
+++ src/perea/conversor/principal/Main.java
@@ -7,15 +7,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //String apiKey="98940a4ca8e53fb94f327ebb";
 
         String menu= """
                 1-Dólar a Peso Argentino
                 2-Dólar a Real Brasilero
                 3-Dólar a Peso Colombiano 
                 4-Peso Colombiano a Dólar
-                5-Invertir conversión
-                6-Salir""";
+                5-Salir""";
 
         System.out.println("Soy la API Conversor de monedas y te doy la bienvenida");
 
@@ -59,7 +57,6 @@ public class Main {
                     System.out.println("Resultado "+tasa2*cantidad);
                     continue;
 
-
                     //break;
 
                 }
@@ -67,19 +64,12 @@ public class Main {
                 }
                 case 3 -> {}
                 case 4 -> {}
-                case 5 -> {}
-                case 6 -> {
+                case 5 -> {
                     System.out.println("Saliendo...");
                     break;
-                    //yield "";
                 }
             }
-
-
-
             break;
         }
-
-
     }
 }
