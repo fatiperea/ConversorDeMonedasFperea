194dd744e8f855b29320c2d3c0e9f5e658ec745c
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-12 11:20:03 -0300

    se seguira el menu propuesto en la consigna

---
+++ src/perea/conversor/principal/Main.java
@@ -0,0 +1,34 @@
+package perea.conversor.principal;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String apiKey="98940a4ca8e53fb94f327ebb";
+
+        String menu= """
+                1-Dólar a Peso Argentino
+                2-Dólar a Real Brasilero
+                3-Dólar a Peso Colombiano 
+                4-Peso Colombiano a Dólar
+                5-Salir""";
+
+
+
+
+        System.out.println("Soy la API Conversor de monedas y te doy la bienvenida");
+
+        Scanner lectura= new Scanner(System.in);
+
+        while (true){
+
+            System.out.println("Selecciona: \\n"+ menu);
+
+
+
+            break;
+        }
+
+
+    }
+}
