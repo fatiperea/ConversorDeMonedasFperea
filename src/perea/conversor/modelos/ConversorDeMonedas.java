22d9bbe81edfa5b312a5b1de5aa18a4fcd06f4c8
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-11 22:54:08 -0300

    agrego getters y setters

--- src/perea/conversor/modelos/ConversorDeMonedas.java
+++ src/perea/conversor/modelos/ConversorDeMonedas.java
@@ -5,4 +5,28 @@ public class ConversorDeMonedas {
     private String monedaBase;
     private String monedaDestino;
     private Double tasaCambio;
+
+    public String getMonedaBase() {
+        return monedaBase;
+    }
+
+    public void setMonedaBase(String monedaBase) {
+        this.monedaBase = monedaBase;
+    }
+
+    public String getMonedaDestino() {
+        return monedaDestino;
+    }
+
+    public void setMonedaDestino(String monedaDestino) {
+        this.monedaDestino = monedaDestino;
+    }
+
+    public Double getTasaCambio() {
+        return tasaCambio;
+    }
+
+    public void setTasaCambio(Double tasaCambio) {
+        this.tasaCambio = tasaCambio;
+    }
 }
