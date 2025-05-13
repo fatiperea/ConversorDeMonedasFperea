7eee570d9c97b76ce4ae75fe5c68c499376bb5ea
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-13 00:50:02 -0300

    creo e implemento la clase

---
+++ src/perea/conversor/modelos/ConexionAPI_Col.java
@@ -0,0 +1,77 @@
+package perea.conversor.modelos;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConexionAPI_Col {
+
+    private String direccion="https://v6.exchangerate-api.com/v6/98940a4ca8e53fb94f327ebb/pair/";
+
+    public double obtenerTasa(String usd, String cop){
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion + "USD" + "/" + "COP"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            //System.out.println(response.body());
+
+            if (response.body() == null || response.body().isEmpty()) {
+                throw new RuntimeException("Error: respuesta vacía.");
+            }
+
+            JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
+
+            if (!jsonObject.has("conversion_rate")) {
+                throw new RuntimeException("Error: No se encontró la tasa de conversión");
+            }
+
+            return jsonObject.get("conversion_rate").getAsDouble();
+
+        }catch (Exception e) {
+            System.out.println("Error al obtener tasa de conversión "+e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+    public double invertir(String ars, String usd) {
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion + "COP" + "/" + "USD"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            //System.out.println(response.body());
+
+            if (response.body() == null || response.body().isEmpty()) {
+                throw new RuntimeException("Error: respuesta vacía.");
+            }
+
+            JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
+
+            if (!jsonObject.has("conversion_rate")) {
+                throw new RuntimeException("Error: No se encontró la tasa de conversión");
+            }
+
+            return jsonObject.get("conversion_rate").getAsDouble();
+
+        } catch (Exception e) {
+            System.out.println("Error al obtener tasa de conversión "+e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
