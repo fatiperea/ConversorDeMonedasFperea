292dc11a3653c680bf9e7fb95ad8f7099fae1847
fatiperea <fatimaperea3pppp0@gmail.com>
2025-05-16 16:58:38 -0300

    implemento caso 5

--- src/perea/conversor/principal/Main.java
+++ src/perea/conversor/principal/Main.java
@@ -13,11 +13,11 @@ public class Main {
         String menu= """
                 1-Dólar a Peso Argentino
                 2-Dólar a Real Brasilero
-                3-Dólar a Peso Colombiano 
-                4-Dólar a Boliviano 
+                3-Dólar a Peso Colombiano
+                4-Dólar a Boliviano
                 5-Peso Argentino a Dólar
                 6-Real Brasilero a Dólar
-                7-Peso Colombiano a Dólar    
+                7-Peso Colombiano a Dólar
                 8-Salir""";
 
         System.out.println("Soy la API Conversor de monedas y te doy la bienvenida");
@@ -30,9 +30,7 @@ public class Main {
 
             int opcion=ValidaEntrada.leerEntero(lectura);
 
-            //ValidaEntrada.rango(lectura,opcion);
-
-            if(opcion>=1 && opcion<=8){
+            if(opcion >= 1 && opcion <= 8){
                 switch (opcion){
                     case 1 -> {
                         System.out.println("Ingrese monto a convertir: ");
@@ -49,8 +47,6 @@ public class Main {
                         System.out.println(cantidad+" pesos argentinos equivalen a "+tasa2*cantidad +" dólares");
                         System.out.println("-----------------------------");
                         continue;
-
-                        //break;
                     }
                     case 2 -> {
                         System.out.println("Ingrese monto a convertir: ");
@@ -103,14 +99,29 @@ public class Main {
                         continue;
 
                     }
+                    case 5 -> {
+                        System.out.println("Ingrese monto a convertir: ");
+
+                        double cantidad = ValidaEntrada.leerDouble(lectura);
+
+                        ConexionAPI_Arg conexion = new ConexionAPI_Arg();
+                        //double tasa = conexion.obtenerTasa("USD","ARS");
+
+                        //System.out.println(cantidad+" dólares equivalen a "+tasa*cantidad +" pesos argentinos");
+
+                        //System.out.println("\nConversión invertida: ");
+                        double tasa2 = conexion.invertir("ARS", "USD");
+                        System.out.println(cantidad+" pesos argentinos equivalen a "+tasa2*cantidad +" dólares");
+                        System.out.println("-----------------------------");
+                        continue;
+                    }
                     case 8 -> {
                         System.out.println("Saliendo...");
-                        break;
                     }
                 }
                 break;
             }else{
-                System.out.println("Ingrese nuevamente:");
+                System.out.println("Ingresa una opción correcta:");
                 lectura.next();
             }
         }
